c5c263cf632ad1a5865083ccc5a28d0839a40bd6
Priyanka Koneru <koneru.priyanka@gmail.com>
2020-02-12 16:27:09 -0800

    reverse string BDD test

--- reverse-string/reverse-string.js
+++ reverse-string/reverse-string.js
@@ -1,3 +1,12 @@
-var reverseString = function() {
-
+var reverseString = function(word) {
+  // first lets understand the input and output, in this case it is a string
+  // create a variable reverse to store that string
+  let reverse = '';
+  // then loop through the string with i as string.length-1, till i >= 0, keep decrementing i
+  for(let i = word.length-1; i >= 0 ; i--) {
+    // print it from reverse, concatenate it to the variable created above
+    reverse += word[i];
+  }
+  // return that variable
+  return reverse;
 };
